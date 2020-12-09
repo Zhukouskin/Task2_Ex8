d15dee7678d59ba3b6d6e768890311a47c2dec9b
Nikita Zhukouski <zhukouskin@gmail.com>
2020-12-09 23:39:40 +0300

    1

--- src/Task2Ex8/Main.java
+++ src/Task2Ex8/Main.java
@@ -10,9 +10,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите число n : ");
         int n = sc.nextInt();
-        int i = 1 ;
         float sum = 0;
-        for(i=1;i<=n;i++)
+        for(int i=1;i<=n;i++)
         {
             sum = sum + (float) 1 / i ;
         }
